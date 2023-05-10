9566a32bf020d7839d9dbd02e14bda542de815b0
aidanaIs <130472015+aidanaIs@users.noreply.github.com>
2023-05-10 22:24:50 +0600

    Update App.java

--- src/main/java/course/App.java
+++ src/main/java/course/App.java
@@ -9,25 +9,8 @@ public class App {
     public static void main(String[] args) {
 
         ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
-<<<<<<< HEAD
-//        Java java= applicationContext.getBean("java", Java.class);
-//        JS js=applicationContext.getBean("js", JS.class);
-//        Course course= applicationContext.getBean("course", Course.class);
-
-//        System.out.println(java);
-//        System.out.println(js);
-//        System.out.println(course);
-
-        Course course=applicationContext.getBean("course", Course.class);
-//        course.getPrgLangs();
-
-        applicationContext.close();
-
-=======
         Dancer dancer = applicationContext.getBean("dancer", Dancer.class);
         System.out.println(dancer);
-
         applicationContext.close();
->>>>>>> bf58fbf (Initial commit)
     }
 }
